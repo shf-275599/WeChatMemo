45bd717dd1f4182738f64541e23c4e4c76b247d0
shf <2755991965@qq.com>
2026-05-10 00:58:44 +0800

    恢复原始导出方式

--- src/exporter/exporter_txt.py
+++ src/exporter/exporter_txt.py
@@ -22,17 +22,15 @@ class TxtExporter(ExporterBase):
         filename = get_new_filename(filename)
         messages = self.database.get_messages(self.contact.wxid, time_range=self.time_range)
         total_steps = len(messages)
+        txt_res = []
+        for index, message in enumerate(messages):
+            if index and index % 1000 == 0:
+                self.update_progress_callback(index / total_steps)
+            if not self.is_selected(message):
+                continue
+            txt_res.append(f'{self.title(message)}\n{message.to_text()}')
         with open(filename, mode='w', newline='', encoding='utf-8') as f:
-            first = True
-            for index, message in enumerate(messages):
-                if index and index % 1000 == 0:
-                    self.update_progress_callback(index / total_steps)
-                if not self.is_selected(message):
-                    continue
-                if not first:
-                    f.write('\n\n')
-                first = False
-                f.write(f'{self.title(message)}\n{message.to_text()}')
+            f.write('\n\n'.join(txt_res))
         self.update_progress_callback(1)
         print(f"【完成导出 TXT {self.contact.remark}】")
         self.finish_callback(self.exporter_id)
